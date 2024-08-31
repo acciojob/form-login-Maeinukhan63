aca362e600974b2b29339bf064d091d58defedf4
Maeinuddin khan <119691264+Maeinukhan63@users.noreply.github.com>
2024-08-31 13:56:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
+    // Prevent the default form submission
+    event.preventDefault();
 
+    // Get the form element
+    const form = document.getElementById('form1');
+
+    // Retrieve the values from the input fields
+    const firstName = form.elements['fname'].value;
+    const lastName = form.elements['lname'].value;
+
+    // Combine first name and last name
+    const fullName = `${firstName} ${lastName}`;
+
+    // Display the result in an alert
+    alert(fullName);
 }
+
+// Attach the getFormvalue function to the form's submit event
+document.getElementById('form1').addEventListener('submit', getFormvalue);
